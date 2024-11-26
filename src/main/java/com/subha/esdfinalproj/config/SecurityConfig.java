aa9f41d5283dfecdade30d311cb7b1d0587f0fef
substobeme <suboff145@gmail.com>
2024-11-26 23:34:16 +0530

    changes made

--- src/main/java/com/subha/esdfinalproj/config/SecurityConfig.java
+++ src/main/java/com/subha/esdfinalproj/config/SecurityConfig.java
@@ -16,7 +16,6 @@ public class SecurityConfig  implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        // Apply the interceptor to all endpoints except /auth/login
         registry.addInterceptor(requestInterceptor)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/api/v1/auth/**","/employee/","/employee");
