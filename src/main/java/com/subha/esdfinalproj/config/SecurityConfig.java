208d8259c2b8cf5853be5944a64aff5d74b98e63
substobeme <suboff145@gmail.com>
2024-11-29 11:53:36 +0530

    removed redundant code

--- src/main/java/com/subha/esdfinalproj/config/SecurityConfig.java
+++ src/main/java/com/subha/esdfinalproj/config/SecurityConfig.java
@@ -1,18 +1,16 @@
 package com.subha.esdfinalproj.config;
 
-import com.subha.esdfinalproj.helper.RequestInterceptor;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig implements WebMvcConfigurer {
-    private final RequestInterceptor requestInterceptor;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
